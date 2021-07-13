49abc275289c00eb5c762b4fc816cb5903376d90
NhanNgo <nngo@axon.com>
2021-07-13 09:47:38 +0700

    Structs part 2

--- Structs/structs.go
+++ Structs/structs.go
@@ -2,13 +2,13 @@ package structs
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
-}
-
-type Circle struct {
-	Radius float64
 }
 
 func (r Rectangle) Perimeter() float64 {
@@ -19,10 +19,19 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height * 1.0
 }
 
+type Circle struct {
+	Radius float64
+}
+
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-type Shape interface {
-	Area() float64
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * 0.5
 }
