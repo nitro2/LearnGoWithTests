641cc8c3ad65bad6fff5e1506bbe85c4c1514faa
NhanNgo <nngo@axon.com>
2021-07-16 09:53:31 +0700

    Dependency Injection part 1'

---
+++ DependencyInjection/di_test.go
@@ -0,0 +1,16 @@
+package di
+
+import "testing"
+import "bytes"
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Nhan")
+
+	got := buffer.String()
+	want := "Hello, Nhan"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
