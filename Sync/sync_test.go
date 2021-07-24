1cab76bcd4548a2ce5018a0e51a2878b9ab59113
NhanNgo <nngo@axon.com>
2021-07-24 21:22:17 +0700

    Sync part 2

--- Sync/sync_test.go
+++ Sync/sync_test.go
@@ -3,9 +3,12 @@ package sync_pkg
 import "testing"
 import "sync"
 
+func NewCounter() *Counter {
+	return &Counter{}
+}
 func TestCounter(t *testing.T) {
 	t.Run("Test increament and retrieve its value", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 
 		counter.Inc()
 		counter.Inc()
@@ -16,7 +19,7 @@ func TestCounter(t *testing.T) {
 
 	t.Run("it runs safely concurrently", func(t *testing.T) {
 		wantedCount := 1000
-		counter := Counter{}
+		counter := NewCounter()
 
 		var wg sync.WaitGroup
 		wg.Add(wantedCount)
@@ -34,7 +37,7 @@ func TestCounter(t *testing.T) {
 
 }
 
-func assertCounter(t testing.TB, got Counter, want int) {
+func assertCounter(t testing.TB, got *Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
 		t.Errorf("got %d, want %d", got.Value(), want)
