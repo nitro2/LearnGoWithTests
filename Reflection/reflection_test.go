09cb3f9d4f2043ee6044d6db5be0373242410140
nitro2 <ngothanhnhannt@gmail.com>
2023-02-27 17:03:15 +0700

    Reflection part 2

--- Reflection/reflection_test.go
+++ Reflection/reflection_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 
 	test_cases := []struct {
@@ -34,6 +44,22 @@ func TestWalk(t *testing.T) {
 				Age  int
 			}{"Chris", 33},
 			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Pointers to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
 		},
 	}
 
