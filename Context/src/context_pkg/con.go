9da422721c9e1f7560d93da98f07d81232835702
nitro2 <ngothanhnhannt@gmail.com>
2023-02-27 17:03:15 +0700

    Context part 3

--- Context/src/context_pkg/con.go
+++ Context/src/context_pkg/con.go
@@ -2,30 +2,23 @@ package context_pkg
 
 import (
 	"fmt"
+	"golang.org/x/net/context"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
+	Fetch(ctx context.Context) (string, error)
 	Cancel()
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+		data, err := store.Fetch(r.Context())
 
-		data := make(chan string, 1)
-
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(w, d)
-		case <-ctx.Done():
-			store.Cancel()
+		if err != nil {
+			return // todo: log error however you like
 		}
 
+		fmt.Fprint(w, data)
 	}
 }
--- Context/src/context_pkg/con_test.go
+++ Context/src/context_pkg/con_test.go
@@ -1,6 +1,8 @@
 package context_pkg
 
 import (
+	// "fmt"
+	"errors"
 	"golang.org/x/net/context"
 	"net/http"
 	"net/http/httptest"
@@ -36,9 +38,31 @@ func (s *SpyStore) assertWasNotCancelled() {
 	}
 }
 
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
+func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
+
+	go func() {
+		var result string
+		for _, c := range s.response {
+			select {
+			case <-ctx.Done():
+				s.t.Log(("spy store cancelled"))
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+				result += string(c)
+			}
+
+		}
+		data <- result
+	}()
+
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-data:
+		return res, nil
+	}
 }
 
 func (s *SpyStore) Cancel() {
@@ -71,11 +95,13 @@ func TestServer(t *testing.T) {
 		time.AfterFunc(5*time.Millisecond, cancel)
 		request = request.WithContext(cancellingCtx)
 
-		response := httptest.NewRecorder()
+		response := &SpyResponseWriter{}
 
 		svr.ServeHTTP(response, request)
 
-		store.assertWasCancelled()
+		if response.written {
+			t.Error("a response should not have been written")
+		}
 	})
 
 	t.Run("returns data from store", func(t *testing.T) {
@@ -94,4 +120,22 @@ func TestServer(t *testing.T) {
 
 		store.assertWasNotCancelled()
 	})
+}
+
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
 }
