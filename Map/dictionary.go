12da8565b01d5219232ae8113934771ace47b010
nitro2 <ngothanhnhannt@gmail.com>
2023-02-27 17:03:15 +0700

    Dictionary part 2

--- Map/dictionary.go
+++ Map/dictionary.go
@@ -1,21 +1,52 @@
 package dictionary
 
-import "errors"
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("word does not exist")
+)
 
-var ErrorNotFound = errors.New("could not find the word you were looking for")
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 type Dictionary map[string]string
 
 func (d Dictionary) Search(s string) (string, error) {
 	definition, ok := d[s]
 	if !ok {
-		return "", ErrorNotFound
+		return "", ErrNotFound
 	}
 
 	return definition, nil
 }
 
-func (d *Dictionary) Add(key, value string) error {
+func (d Dictionary) Add(key, value string) error {
+	if d[key] != "" {
+		return ErrWordExists
+	}
 	d[key] = value
 	return nil
+}
+
+func (d Dictionary) Update(key, value string) error {
+	_, err := d.Search(key)
+	if err == ErrNotFound {
+		return ErrWordDoesNotExist
+	}
+	d[key] = value
+	return nil
+}
+
+func (d Dictionary) Delete(key string) error {
+	_, err := d.Search(key)
+	if err != nil {
+		return err
+	}
+
+	delete(d, key)
+	return nil
+
 }
